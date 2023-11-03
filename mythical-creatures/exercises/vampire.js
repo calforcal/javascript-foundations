2e1388aec808b193ba3368d2fe5bc2516ef6232f
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2023-11-03 09:02:23 -0400

    Add functionality for vampire class

--- mythical-creatures/exercises/vampire.js
+++ mythical-creatures/exercises/vampire.js
@@ -1,9 +1,39 @@
+const createVampire = (name, pet="bat") => {
+  const Vampire = {
+    name: name,
+    pet: pet,
+    get thirsty() {
+      return this.ouncesDrank > 49 ? false : true;
+    },
+    ouncesDrank: 0,
+  }
+  return Vampire;
+}
 
+const encounterDeliciousVictim = (vampire) => {
+  return vampire.thirsty ? "I WANT TO SUCK YOUR BLOOD!" : "No thanks, I am too full."
+}
+
+const drink = (vampire) => {
+  if (vampire.thirsty) {
+    vampire.ouncesDrank += 10;
+  }
+  return vampire;
+}
+
+const inquirePlace = (locations, place) => {
+  return locations.includes(place) ? `Yes, I have spent some time in ${place}.` : `No, I have never been to ${place}.`
+}
+
+const findBatLovers = (vampires) => {
+  bats = vampires.filter((vampire) => vampire.pet === "bat")
+  return bats.map((vampire) => vampire.name)
+}
 
 module.exports = {
-  // createVampire, 
-  // drink, 
-  // findBatLovers, 
-  // encounterDeliciousVictim, 
-  // inquirePlace
+  createVampire, 
+  drink, 
+  findBatLovers, 
+  encounterDeliciousVictim, 
+  inquirePlace
 }
