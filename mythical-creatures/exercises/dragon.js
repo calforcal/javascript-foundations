a3b53dad84005f81c5f72c2ba545062148dfdde6
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2023-11-02 21:14:49 -0400

    Add all functions for Dragon class

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,14 +1,32 @@
-const createDragon = (name) => {
+const createDragon = (name, rider, temperment) => {
   const Dragon = {
     name: name,
+    rider: rider,
+    temperment: temperment,
+    timesEaten: 0,
+    hungry: true,
   }
 
   return Dragon
 }
 
+const greetRider = (dragon) => {
+  return `Hi, ${dragon.rider}!`
+}
+
+const eat = (dragon) => {
+  dragon.timesEaten += 1
+  dragon.timesEaten >= 3 ? dragon.hungry = false : dragon.hungry = true
+  return dragon
+}
+
+const findFireBreathers = (dragons) => {
+  return dragons.filter((dragon) => dragon.temperment === "aggressive");
+}
+
 module.exports = {
   createDragon, 
-  // greetRider, 
-  // eat, 
-  // findFireBreathers
+  greetRider, 
+  eat,
+  findFireBreathers
 }
