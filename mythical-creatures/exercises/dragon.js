f2c4a1f9e8e14b5f7dc032204e9af9d23f81ab3a
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2023-11-02 20:26:15 -0400

    Add createDragon method

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,7 +1,13 @@
+const createDragon = (name) => {
+  const Dragon = {
+    name: name,
+  }
 
+  return Dragon
+}
 
 module.exports = {
-  // createDragon, 
+  createDragon, 
   // greetRider, 
   // eat, 
   // findFireBreathers
