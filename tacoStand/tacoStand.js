c0f0eba878cc533f89d61de0788569c2c88f4131
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2023-11-04 18:12:01 -0400

    Add functionality for tacoStand module

--- tacoStand/tacoStand.js
+++ tacoStand/tacoStand.js
@@ -1,2 +1,35 @@
+const createIngredient = (name="unknown", price=0.00) => {
+  const Ingredient = {
+    name: name,
+    price: price,
+  }
+  return Ingredient
+}
 
-module.exports = {}
+const createTaco = (name="custom", ingredients=[]) => {
+  const Taco = {
+    name: name,
+    ingredients: ingredients
+  }
+  return Taco
+}
+
+const addIngredientToTaco = (taco, ingredient=null) => {
+  if (ingredient !== null) {
+    taco.ingredients.push(ingredient)
+  }
+  return taco
+}
+
+const calculatePrice = (taco) => {
+  sum = 0.00
+  taco.ingredients.forEach((ingredient) => sum += ingredient.price)
+  return sum
+}
+
+module.exports = {
+  createIngredient,
+  createTaco,
+  addIngredientToTaco,
+  calculatePrice,
+}
