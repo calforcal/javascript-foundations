4540a7f44588e5a25bbc5fa68cbfd04e4c83ab4b
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2023-11-04 17:14:04 -0400

    Add functionality for the vendingMachine module

--- vending-machine/vendingMachine.js
+++ vending-machine/vendingMachine.js
@@ -1,2 +1,32 @@
+const createItemStock = (name="unknown", quantity=0, price=1.00) => {
+  const Item = {
+    name: name,
+    quantity: quantity,
+    price: price,
+  }
+  return Item
+}
 
-module.exports = {}
+const makePurchase = (item, moneyGiven) => {
+  if (item.price > moneyGiven) {
+    return `Sorry, you need at least $${Math.round(item.price)} to make that purchase`
+  }
+  else if (item.quantity <= 0) {
+    return `Sorry, there are none left`
+  }
+  else {
+    return `Here are your ${item.name}`
+  }
+}
+
+const collectChange = (changeGiven) => {
+  let sum = 0.0
+  changeGiven.forEach((coin) => sum += coin)
+  return sum
+}
+
+module.exports = {
+  createItemStock,
+  makePurchase,
+  collectChange,
+}
