3b7b7b81c9547d74b73870e54f6bd398f03ac359
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2023-11-04 18:00:22 -0400

    Add functionality for library module

--- library/library.js
+++ library/library.js
@@ -1,3 +1,19 @@
+const issueCard = (name, age, numBooksCheckedOut=0) => {
+  const LibraryCard = {
+    name: name,
+    age: age,
+    numBooksCheckedOut: numBooksCheckedOut,
+    get isChild() { return this.age < 12 ? true : false },
+  }
+  return LibraryCard
+}
 
+const searchByAuthor = (collection, authorName) => {
+  let results = collection.filter((book) => book.author === authorName)
+  return results.length === 0 ? "No book found for search criteria" : results
+}
 
-module.exports = { };
+module.exports = {
+  issueCard,
+  searchByAuthor,
+};
