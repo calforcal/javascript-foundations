7c04b79e58b54ecb58c27f168aeb6b81ced2a113
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2023-11-04 17:48:15 -0400

    Add functionality for elevator module

--- elevator/elevator.js
+++ elevator/elevator.js
@@ -1,3 +1,32 @@
+const createElevator = (building, floors, currentFloor, passengers) => {
+  const Elevator = {
+    building: building,
+    floors: floors,
+    currentFloor: currentFloor,
+    passengers: passengers
+  }
+  return Elevator
+}
 
+const changeFloors = (elevator, newFloor) => {
+  if (elevator.currentFloor === newFloor) {
+    return `You're already on floor ${newFloor}!`
+  }
+  else if (newFloor < 1 || newFloor > elevator.floors) {
+    return `Floor ${newFloor} does not exist!`
+  }
+  else {
+    elevator.currentFloor = newFloor
+    return `Taking you to floor ${newFloor}!`
+  }
+}
 
-module.exports = {  };
+const dropOffPassenger = (elevator, passenger) => {
+  return elevator.passengers.filter((person) => person !== passenger)
+}
+
+module.exports = {
+  createElevator,
+  changeFloors,
+  dropOffPassenger,
+};
