70724bc18075c6f7b0cf8ff78b27f057e58efc1d
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2023-11-03 21:23:53 -0400

    Add functionality for foods module

--- favorite-foods/favorite-foods.js
+++ favorite-foods/favorite-foods.js
@@ -1,4 +1,35 @@
+const createFavoriteFood = (food) => {
+  const Food = {
+    name: food.dish,
+    ingredients: food.ingredients,
+    isSpicy: food.isSpicy,
+    timesOrdered: 0,
+  }
+  return Food;
+}
 
+const commentOnSpiciness = (food) => {
+  if (food.isSpicy) {
+    return `Wow, this ${food.name} is so spicy!`
+  } else {
+    return `Phew, this ${food.name} is not very spicy.`
+  }
+}
 
+const orderFood = (food) => {
+  food.timesOrdered += 1
+  return food
+}
 
-module.exports = { };
+const createShoppingList = (foods) => {
+  const all_ingredients = []
+  foods.forEach((food) => all_ingredients.push(food.ingredients))
+  return all_ingredients.flat()
+}
+
+module.exports = {
+  createFavoriteFood,
+  commentOnSpiciness,
+  orderFood,
+  createShoppingList,
+};
