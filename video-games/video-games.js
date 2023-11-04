db2bd4dd88497440d5dca97553b5373ceeaa60d0
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2023-11-04 08:26:43 -0400

    Add functionality for video games module

--- video-games/video-games.js
+++ video-games/video-games.js
@@ -1,4 +1,48 @@
+const createPlayer = (name, age, moveset=[]) => {
+  const Player = {
+    name: name,
+    age: age,
+    moveset: moveset,
+  }
+  return Player
+}
 
+const createLevel = (name, players=[]) => {
+  const Level = {
+    name: name,
+    players: players,
+    coins: 0,
+    lives: 3,
+    nextLife: 0,
+  }
+  return Level
+}
 
+const findCoins = (level, numCoins) => {
+  level.coins += numCoins
+  level.nextLife += numCoins
 
-module.exports = { };
+  if (level.nextLife >= 100) {
+    level.nextLife - 100
+    level.lives += 1
+  } 
+
+  return level
+}
+
+const defeatPlayer = (level) => {
+  level.lives -= 1
+
+  if (level.lives < 1) {
+    return "GAME OVER"
+  } else {
+    return level
+  }
+}
+
+module.exports = {
+  createPlayer,
+  createLevel,
+  findCoins,
+  defeatPlayer,
+};
