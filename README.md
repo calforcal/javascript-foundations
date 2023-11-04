808f99cdda11965876e6da8a3e4e30ac903cd2a5
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2023-11-04 16:51:03 -0400

    Add functionality for calendar module

--- README.md
+++ README.md
@@ -47,7 +47,7 @@ Completing all of these test suites is not necessary, but you should be working 
 - [X] 🎧 [DJ](./dj)
 - [X] 🍔 [Favorite Foods](./favorite-foods)
 - [X] 🎮 [Video Games](./video-games/)
-- [ ] 🎂 [Birthdays](./birthdays)
+- [X] 🎂 [Birthdays](./birthdays)
 - [ ] 🗓 [Calendar](./calendar/)
 - [ ] 💵 [Vending Machine](./vending-machine/)
 - [ ] 🛗 [Elevator](./elevator/)
--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,3 +1,30 @@
+const createEvent = (title, month, day) => {
+  if (day > 31 || day < 1) {
+    return `Error: ${day} is not a valid day`
+  }
+  const Event = {
+    title: title,
+    month: month,
+    day: day,
+  }
+  return Event
+}
 
+const createCalendar = (owner, events) => {
+  const Calendar = {
+    owner: owner,
+    events: events,
+  }
+  return Calendar
+}
 
-module.exports = {  };
+const reportMonthlyEvents = (calendar, month) => {
+  monthsEvents = calendar.events.filter((event) => event.month === month)
+  return monthsEvents
+}
+
+module.exports = {
+  createEvent,
+  createCalendar,
+  reportMonthlyEvents,
+};
