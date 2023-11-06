b3b2bf46d1eeb9cb16ccf2e1974fff12099e64e8
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2023-11-06 09:33:55 -0700

    Add functionality for Spa Module

--- spa/spa.js
+++ spa/spa.js
@@ -1,8 +1,45 @@
+const createCustomer = (name, bill, bookings=[]) => {
+  const Customer = {
+    name: name,
+    bill: bill,
+    bookings: bookings,
+  }
+  return Customer
+}
+
+const greeting = (customer) => {
+  if (customer.bookings.length > 0) {
+    return `${customer.name}! Welcome back to Happy Spa`
+  }
+  else {
+    return `${customer.name}! Welcome to Happy Spa`
+  }
+}
+
+const createService = (name, cost) => {
+  if (name !== undefined && cost !== undefined){
+    return { name: name, cost: cost }
+  }
+  else {
+    return 'Please provide service name and cost.'
+  }
+}
+
+const bookServices = (customer, service) => {
+  customer.bookings.push(service.name)
+  customer.bill += service.cost
+  return customer
+}
+
+const applyGiftCard = (services, giftCardAmount) => {
+  var affordableServices = services.filter((service) => giftCardAmount >= service.price)
+  return affordableServices.map((service) => service.name)
+}
 
 module.exports = { 
-  // createCustomer, 
-  // greeting, 
-  // createService,
-  // bookServices, 
-  // applyGiftCard 
+  createCustomer, 
+  greeting, 
+  createService,
+  bookServices, 
+  applyGiftCard 
 }
