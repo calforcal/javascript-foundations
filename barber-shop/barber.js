a26728a5f42c389437180f60375a250d528a2b14
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2023-11-06 07:52:50 -0700

    Add functionality for barber module

--- barber-shop/barber.js
+++ barber-shop/barber.js
@@ -1,3 +1,30 @@
+const createBarber = (name, earnings=0, haircuts=[]) => {
+  const Barber = {
+    name: name,
+    earnings: earnings,
+    haircuts: haircuts
+  }
+  return Barber
+}
 
+const giveCompliment = (haircut) => {
+  return `This ${haircut.style} looks great!`
+}
 
-module.exports = {  };
+const cutHair = (barber, haircut) => {
+  barber.haircuts.push(haircut)
+  barber.earnings += haircut.price
+  return barber
+}
+
+const listStyles = (barber, styleWanted) => {
+  var styles = barber.haircuts.filter((cut) => cut.hairLength === styleWanted)
+  return styles.map((cut) => cut.style)
+}
+
+module.exports = {
+  createBarber,
+  giveCompliment,
+  cutHair,
+  listStyles,
+};
